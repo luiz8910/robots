2223fb97764241e4c54ff33466b492306a6d4ef2
Luiz Fernando Sanches <luiz.sanches8910@gmail.com>
2016-05-18 16:00:07 -0300

    Commit Inicial

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Admin\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface UserRepository
+ * @package namespace Admin\Repositories;
+ */
+interface UserRepository extends RepositoryInterface
+{
+    //
+}
---
+++ app/Repositories/UserRepositoryEloquent.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Admin\Repositories;
+
+use Prettus\Repository\Eloquent\BaseRepository;
+use Prettus\Repository\Criteria\RequestCriteria;
+use Admin\Repositories\UserRepository;
+use Admin\Models\User;
+use Admin\Validators\UserValidator;
+
+/**
+ * Class UserRepositoryEloquent
+ * @package namespace Admin\Repositories;
+ */
+class UserRepositoryEloquent extends BaseRepository implements UserRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return User::class;
+    }
+
+    
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
+}
